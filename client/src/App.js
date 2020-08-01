261c8434511b61cb9efc74ffad08fd1e52609d09
tony <tonyz0212@gmail.com>
2020-07-31 22:14:21 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href = 'http://localhost:8888'>
+      <button> Login with Spotify </button>
+
+      </a>
+    </div>
+  );
+}
+
+export default App;
