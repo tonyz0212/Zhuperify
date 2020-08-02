6746067539075a48871c453b0e13ba7ad93c8b66
tony <tonyz0212@gmail.com>
2020-08-01 20:45:55 -0700

    working on the play song api

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Spotify from 'spotify-web-api-js';
 
-function App() {
-  return (
-    <div className="App">
-      <a href = 'http://localhost:8888'>
-      <button> Login with Spotify </button>
+const spotifyWebApi = new Spotify();
 
-      </a>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    const params = this.getHashParams();
+    this.state ={
+      loggedIn : params.access_token ? true: false,
+      nowPlaying:{
+        name: 'idk',
+        image: 'not yet'
+       }
+    }
+    if( params.access_token){
+      spotifyWebApi.setAccessToken(params.access_token)
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying(){
+    spotifyWebApi.getMyCurrentPlaybackState()
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <a href='http://localhost:8888' > Login to Spotify </a>
+        <div> Now Playing: {this.state.nowPlaying.name} </div>
+        <div> {<img src={this.state.nowPlaying.albumArt} style={{ height: 150 }}/> } </div>
+        
+        <button onClick={ ()=>this.getNowPlaying()}> Check now playing </button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
